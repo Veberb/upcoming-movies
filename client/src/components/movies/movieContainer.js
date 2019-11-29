fb746f737bbcb5bc6e8993fe0fd52de287ddf038
Lucas Veber de Brida <lucasveberdebrida@gmail.com>
2019-11-28 22:21:12 -0200

    asdjaadsidshaopas

--- client/src/components/movies/movieContainer.js
+++ client/src/components/movies/movieContainer.js
@@ -7,11 +7,16 @@ import './movieContainer.css';
 
 function MovieContainer() {
   const [movies, setMovies] = useState([]);
-  const page = useRef(1);
+  const page = useRef(0);
   const hasMoreToLoad = useRef(true);
+  const isFetching = useRef(false);
 
   const getList = async () => {
+    if (isFetching.current) return;
+    isFetching.current = true;
+    console.log(page.current);
     const { data } = await axios.get('movies', { params: { page: page.current } });
+    isFetching.current = false;
     setMovies(old => [...old, ...data.results]);
     hasMoreToLoad.current = data.page === data.total_pages;
   };
@@ -25,7 +30,6 @@ function MovieContainer() {
     getList();
   }, []);
 
-  //loadMore={loadFunc}
   return (
     <div>
       <div className="search-bar">
@@ -35,6 +39,7 @@ function MovieContainer() {
         pageStart={0}
         hasMore={hasMoreToLoad.current}
         loadMore={loadMoreMovies}
+        threshold={100}
         loader={
           <div className="ui active inverted dimmer">
             <div className="ui large text loader">Loading</div>
